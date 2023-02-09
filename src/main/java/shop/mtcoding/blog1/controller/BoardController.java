c0661dc6f99bef086e7448a450e9dfc31edad82d
DH <shim5013@naver.com>
2023-02-09 16:15:11 +0900

    break time

--- src/main/java/shop/mtcoding/blog1/controller/BoardController.java
+++ src/main/java/shop/mtcoding/blog1/controller/BoardController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import shop.mtcoding.blog1.model.BoardRepository;
 
@@ -17,5 +18,11 @@ public class BoardController {
     public String main(Model model) {
         model.addAttribute("dtos", boardRepository.findAllWithUser());
         return "board/main";
+    }
+
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable int id, Model model) {
+        model.addAttribute("dto", boardRepository.findByIdWithUser(id));
+        return "board/detail";
     }
 }
